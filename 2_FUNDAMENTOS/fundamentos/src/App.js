0c14e91ea2280f148f79233f89e833fabbfd5c88
Roger <rogerbritosan@gmail.com>
2023-01-27 17:46:33 -0300

    adicionando comentários

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+// componentes
+import FirstComponent from './components/FirstComponent'
+
+// estilos
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos do React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,23 @@
+// estilos
+
+const FirstComponent = () => {
+    // comentários podem vir aqui
+
+    /*
+    várias linhas
+    várias linhas
+    várias linhas
+    várias linhas
+    várias linhas
+    */
+
+    return(        
+        <div>
+            {/* mais comentários */}
+            <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
+        </div>
+    )
+}
+
+export default FirstComponent
